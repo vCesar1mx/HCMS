34b35b740f79f173d1c369db1be468253bfec95e
vCesar1mx <cesarvaz.e7@gmail.com>
2021-10-23 17:22:13 -0500

    v1.0.1.2

---
+++ engine/corecss.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="./engine/css/theme-blank.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="https://mdbootstrap.com/api/snippets/static/download/MDB5-Pro-Advanced_3.3.0/plugins/css/all.min.css">
+<script src="./engine/js/fastcss.js"></script>
+<script src="https://mdbootstrap.com/wp-content/themes/mdbootstrap4/js/mdb5/3.9.0/compiled.min.js"></script>
---
+++ template/navbar.php
@@ -0,0 +1,37 @@
+        <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+          <!-- Container wrapper -->
+          <div class="container">
+            <!-- Navbar brand -->
+            <a class="navbar-brand me-2" href="https://mdbgo.com/">
+              <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" height="16" alt="" loading="lazy" style="margin-top: -1px;">
+            </a>
+
+            <!-- Toggle button -->
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false" aria-label="Toggle navigation">
+              <i class="fas fa-bars"></i>
+            </button>
+
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse text-center" id="navbarButtonsExample">
+              <!-- Left links -->
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><?php echo $servername;?></a>
+                </li>
+              </ul>
+              <!-- Left links -->
+
+              <div class="d-flex text-center" style="justify-content:center;">
+                <button type="button" class="btn btn-dark px-3 me-2">
+                  Login
+                </button>
+                <button type="button" class="btn btn-primary me-3">
+                  Registro
+                </button>
+                <a class="btn btn-dark px-3" href="https:/discord.haliacraft.com" role="button"><i class="fab fa-discord"></i></a>
+              </div>
+            </div>
+            <!-- Collapsible wrapper -->
+          </div>
+          <!-- Container wrapper -->
+        </nav>
